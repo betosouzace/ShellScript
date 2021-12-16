60a11599bbe9e80dc9f7fd49c4fb03d5890242d3
alvesbeto <beto.souza@corbee.com.br>
2021-12-15 21:50:01 -0300

    automatizado instalaçao XAMPP

--- MintInstall.sh
+++ MintInstall.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if (( ! $EUID != 0 )); then
+clear
+echo "                       !!! ATENÇÃO !!!"
+echo "              NÃO execute este script como root!"
+echo "              Presione qualquer tecla para sair..."
+read -s -n 1 -p " "
+exit
+fi
+
 # ----------------------------- VARIÁVEIS ----------------------------- #
 GIT_USER="seuUsuarioGit"
 GIT_EMAIL="seuemailgit@email.com"
@@ -106,13 +115,6 @@ PROGRAMAS_PARA_INSTALAR=(
   npm
 )
 # ---------------------------------------------------------------------- #
-
-clear
-echo "                       !!! ATENÇÃO !!!"
-echo "              NÃO execute este script como root!"
-echo "      Presione Ctrl+C para sair ou Enter para continuar..."
-read -s -n 1 -p " "
-echo " "
 
 # ----------------------------- REQUISITOS ----------------------------- #
 ## Removendo travas eventuais do apt ##
@@ -233,6 +235,7 @@ sudo systemctl disable apache2
 sudo apt remove apache2 -y
 
 ## Instalacao XAMPP 5.6 #
+if [ ! -d "/opt/lampp/etc" ]; then
 wget https://ufpr.dl.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.40/xampp-linux-x64-5.6.40-1-installer.run
 sudo chmod 755 xampp-linux-x64-5.6.40-1-installer.run
 sudo ./xampp-linux-x64-5.6.40-1-installer.run --mode text --installer-language pt_BR
@@ -241,9 +244,36 @@ sudo ln -s /opt/lampp/htdocs/ $PASTA_USUARIO/htdocs
 sudo chmod 777 -R $PASTA_USUARIO/htdocs
 sudo sed -i 's/display_errors=On/display_errors=Off/g' /opt/lampp/etc/php.ini
 sudo sed -i 's/short_open_tag=Off/short_open_tag=On/g' /opt/lampp/etc/php.ini
-
+rm -f $PASTA_USUARIO/.local/share/applications/XAMPP.desktop
+rm -f $PASTA_USUARIO/Desktop/XAMPP.desktop
+rm -f "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
+sudo rm -f /usr/share/applications/XAMPP.desktop
+cat >"$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" <<EOT
+[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+Name=XAMPP Control Panel
+Comment=Start and Stop XAMPP
+Exec=sudo /opt/lampp/manager-linux-x64.run
+Icon=/opt/lampp/htdocs/favicon.ico
+Categories=Development
+Type=Application
+Terminal=true
+EOT
+chown $USER:$USER -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
+chmod 755 -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
+ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" $PASTA_USUARIO/Desktop/XAMPP.desktop
+ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
+sudo cp "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" /usr/share/applications/XAMPP.desktop
+sudo chmod 755 -R /usr/share/applications/XAMPP.desktop
+echo "XAMPP Instalado com sucesso!"
 ## Crinado Link simbólico Mysql #
 sudo ln -s /opt/lampp/bin/mysql /usr/bin/mysql
+# sudo /opt/lampp/lampp start
+# xdg-open http://localhost
+else
+echo "Já exite uma instalação do XAMPP"
+fi
 
 ## Apagando pasta de Downloads #
 sudo rm -rf "$DIRETORIO_DOWNLOADS"
--- XamppInstall.sh
+++ XamppInstall.sh
@@ -1,58 +1,64 @@
 #!/usr/bin/env bash
 
+if (( ! $EUID != 0 )); then
+clear
+echo "                       !!! ATENÇÃO !!!"
+echo "              NÃO execute este script como root!"
+echo "              Presione qualquer tecla para sair..."
+read -s -n 1 -p " "
+exit
+fi
+
 PASTA_USUARIO=$HOME
 
 ## Instalacao XAMPP 5.6 #
-# wget https://ufpr.dl.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.40/xampp-linux-x64-5.6.40-1-installer.run
-# sudo chmod 755 xampp-linux-x64-5.6.40-1-installer.run
-# sudo ./xampp-linux-x64-5.6.40-1-installer.run --mode text --installer-language pt_BR
-# sudo rm xampp-linux*
-# sudo ln -s /opt/lampp/htdocs/ $HOME/htdocs
-# sudo chmod 777 -R $HOME/htdocs
-# sudo sed -i 's/display_errors=On/display_errors=Off/g' /opt/lampp/etc/php.ini
-# sudo sed -i 's/short_open_tag=Off/short_open_tag=On/g' /opt/lampp/etc/php.ini
+if [ ! -d "/opt/lampp/etc" ]; then
 
-echo $PASTA_USUARIO
+wget https://ufpr.dl.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.40/xampp-linux-x64-5.6.40-1-installer.run
+sudo chmod 755 xampp-linux-x64-5.6.40-1-installer.run
+sudo ./xampp-linux-x64-5.6.40-1-installer.run --mode text --installer-language pt_BR
+sudo rm xampp-linux*
+sudo ln -s /opt/lampp/htdocs/ $PASTA_USUARIO/htdocs
+sudo chmod 777 -R $PASTA_USUARIO/htdocs
+sudo sed -i 's/display_errors=On/display_errors=Off/g' /opt/lampp/etc/php.ini
+sudo sed -i 's/short_open_tag=Off/short_open_tag=On/g' /opt/lampp/etc/php.ini
 
-cat >"$PASTA_USUARIO/XAMPP.desktop" <<EOT
+rm -f $PASTA_USUARIO/.local/share/applications/XAMPP.desktop
+rm -f $PASTA_USUARIO/Desktop/XAMPP.desktop
+rm -f "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
+sudo rm -f /usr/share/applications/XAMPP.desktop
+
+cat >"$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" <<EOT
 [Desktop Entry]
-Name=XAMPP Control Panel
-Exec=sudo /opt/lampp/manager-linux-x64.run
-Comment=
-Terminal=true
-Icon=/opt/lampp/htdocs/favicon.ico
-Categories=Development;
+Encoding=UTF-8
 Type=Application
+Name=XAMPP Control Panel
+Comment=Start and Stop XAMPP
+Exec=sudo /opt/lampp/manager-linux-x64.run
+Icon=/opt/lampp/htdocs/favicon.ico
+Categories=Development
+Type=Application
+Terminal=true
 EOT
 
-# cat >'/usr/share/applications/XAMPP.desktop' <<EOT
-# [Desktop Entry]
-# Name=XAMPP Control Panel
-# Exec=sudo /opt/lampp/manager-linux-x64.run
-# Comment=
-# Terminal=true
-# Icon=/opt/lampp/htdocs/favicon.ico
-# Categories=Development;
-# Type=Application
-# EOT
+chown $USER:$USER -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
+chmod 755 -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
+ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" $PASTA_USUARIO/Desktop/XAMPP.desktop
+ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
+sudo cp "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" /usr/share/applications/XAMPP.desktop
+sudo chmod 755 -R /usr/share/applications/XAMPP.desktop
 
-
-# rm -f $HOME/mi_app/mi_app.desktop
-# rm -f $HOME/.local/share/applications/mi_app.desktop
-# rm -f $HOME/Desktop/mi_app.desktop
-# rm -f $HOME/Escritorio/mi_app.desktop
-# rm -f /usr/share/applications/mi_app.desktop
-
-# chown $USER:$USER -R `echo $HOME`/mi_app/mi_app.desktop
-
-# chmod 755 `echo $HOME`/mi_app/mi_app.desktop
-
-# su - $USER -c "xdg-open 'https://www.mi-app.com/'"
+echo "XAMPP Instalado com sucesso!"
 
 ## Crinado Link simbólico Mysql #
-# sudo ln -s /opt/lampp/bin/mysql /usr/bin/mysql
+sudo ln -s /opt/lampp/bin/mysql /usr/bin/mysql
 
-# clear
-echo "XAMPP Instalado com sucesso!"
-read -s -n 1 -p "Pressione Enter para sair..."
+# sudo /opt/lampp/lampp start
+# xdg-open http://localhost
+
+else
+echo "Já exite uma instalação do XAMPP"
+fi
+
+read -s -n 1 -p "Pressione alguma tecla para sair..."
 exit
