41f6844f19bc53fa3f48e82daaad499099b0bd3c
alvesbeto <beto.souza@corbee.com.br>
2021-12-15 22:11:36 -0300

    formacaçao codigo

--- UbuntuBasedInstall.sh
+++ UbuntuBasedInstall.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if (( ! $EUID != 0 )); then
-clear
-echo "                       !!! ATENÇÃO !!!"
-echo "              NÃO execute este script como root!"
-echo "              Presione qualquer tecla para sair..."
-read -s -n 1 -p " "
-exit
+if [[ $EUID -eq 0 ]]; then
+  clear
+  echo "                       !!! ATENÇÃO !!!"
+  echo "              NÃO execute este script como root!"
+  echo "              Presione qualquer tecla para sair..."
+  read -s -n 1 -p " "
+  exit
 fi
 
 # ----------------------------- VARIÁVEIS ----------------------------- #
@@ -134,7 +134,6 @@ curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-
 ## Adicionando repositórios de terceiros e suporte a Snap (Driver Logitech, Lutris e Drivers Nvidia)##
 sudo add-apt-repository "$PPA_GIMP" -y
 sudo add-apt-repository "$PPA_INKS" -y
@@ -153,13 +152,13 @@ sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo ap
 
 ## Download e instalaçao de programas externos ##
 mkdir "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_GOOGLE_CHROME" -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_4K_VIDEO_DOWNLOADER" -P "$DIRETORIO_DOWNLOADS"
-wget -c "$ANYDESK"                 -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_FDM"                 -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_VCODE"               -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_SKYPE"               -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_WORKB"               -P "$DIRETORIO_DOWNLOADS"
+wget -c "$ANYDESK" -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_FDM" -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_VCODE" -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_SKYPE" -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_WORKB" -P "$DIRETORIO_DOWNLOADS"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
@@ -235,19 +234,19 @@ sudo apt remove apache2 -y
 
 ## Instalacao XAMPP 5.6 #
 if [ ! -d "/opt/lampp/etc" ]; then
-wget https://ufpr.dl.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.40/xampp-linux-x64-5.6.40-1-installer.run
-sudo chmod 755 xampp-linux-x64-5.6.40-1-installer.run
-sudo ./xampp-linux-x64-5.6.40-1-installer.run --mode text --installer-language pt_BR
-sudo rm xampp-linux*
-sudo ln -s /opt/lampp/htdocs/ $PASTA_USUARIO/htdocs
-sudo chmod 777 -R $PASTA_USUARIO/htdocs
-sudo sed -i 's/display_errors=On/display_errors=Off/g' /opt/lampp/etc/php.ini
-sudo sed -i 's/short_open_tag=Off/short_open_tag=On/g' /opt/lampp/etc/php.ini
-rm -f $PASTA_USUARIO/.local/share/applications/XAMPP.desktop
-rm -f $PASTA_USUARIO/Desktop/XAMPP.desktop
-rm -f "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
-sudo rm -f /usr/share/applications/XAMPP.desktop
-cat >"$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" <<EOT
+  wget https://ufpr.dl.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.40/xampp-linux-x64-5.6.40-1-installer.run
+  sudo chmod 755 xampp-linux-x64-5.6.40-1-installer.run
+  sudo ./xampp-linux-x64-5.6.40-1-installer.run --mode text --installer-language pt_BR
+  sudo rm xampp-linux*
+  sudo ln -s /opt/lampp/htdocs/ $PASTA_USUARIO/htdocs
+  sudo chmod 777 -R $PASTA_USUARIO/htdocs
+  sudo sed -i 's/display_errors=On/display_errors=Off/g' /opt/lampp/etc/php.ini
+  sudo sed -i 's/short_open_tag=Off/short_open_tag=On/g' /opt/lampp/etc/php.ini
+  rm -f $PASTA_USUARIO/.local/share/applications/XAMPP.desktop
+  rm -f $PASTA_USUARIO/Desktop/XAMPP.desktop
+  rm -f "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
+  sudo rm -f /usr/share/applications/XAMPP.desktop
+  cat >"$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" <<EOT
 [Desktop Entry]
 Encoding=UTF-8
 Type=Application
@@ -259,19 +258,19 @@ Categories=Development
 Type=Application
 Terminal=true
 EOT
-chown $USER:$USER -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
-chmod 755 -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
-ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" $PASTA_USUARIO/Desktop/XAMPP.desktop
-ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
-sudo cp "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" /usr/share/applications/XAMPP.desktop
-sudo chmod 755 -R /usr/share/applications/XAMPP.desktop
-echo "XAMPP Instalado com sucesso!"
-## Crinado Link simbólico Mysql #
-sudo ln -s /opt/lampp/bin/mysql /usr/bin/mysql
+  chown $USER:$USER -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
+  chmod 755 -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
+  ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" $PASTA_USUARIO/Desktop/XAMPP.desktop
+  ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
+  sudo cp "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" /usr/share/applications/XAMPP.desktop
+  sudo chmod 755 -R /usr/share/applications/XAMPP.desktop
+  echo "XAMPP Instalado com sucesso!"
+  ## Crinado Link simbólico Mysql #
+  sudo ln -s /opt/lampp/bin/mysql /usr/bin/mysql
 # sudo /opt/lampp/lampp start
 # xdg-open http://localhost
 else
-echo "Já exite uma instalação do XAMPP"
+  echo "Já exite uma instalação do XAMPP"
 fi
 
 ## Apagando pasta de Downloads #
