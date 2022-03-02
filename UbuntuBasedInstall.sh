ee33265d446a7e83b134c99a11a9f0533abf1e38
alvesbeto <beto.souza@corbee.com.br>
2022-03-02 00:58:40 -0300

    novo arquivo instalação UbuntuBased

--- UbuntuBasedInstall.sh
+++ UbuntuBasedInstall.sh
@@ -27,96 +27,11 @@ URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_curre
 URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.18.5-1_amd64.deb"
 URL_ANYDESK="https://download.anydesk.com/linux/anydesk_6.1.1-1_amd64.deb"
 URL_FDM="https://dn3.freedownloadmanager.org/6/latest/freedownloadmanager.deb"
-URL_VCODE="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 URL_SKYPE="https://go.skype.com/skypeforlinux-64.deb"
 URL_WORKB="https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community_8.0.27-1ubuntu20.04_amd64.deb"
 
 DIRETORIO_DOWNLOADS="$PASTA_USUARIO/Downloads/programas"
 
-PROGRAMAS_PARA_INSTALAR=(
-  dos2unix
-  language-pack-gnome-pt
-  language-pack-kde-pt
-  language-pack-pt-base
-  winff
-  guvcview
-  flameshot
-  steam-installer
-  steam-devices
-  steam:i386
-  ratbagd
-  gimp
-  gimp-gmic
-  gmic
-  gimp-plugin-registry
-  p7zip-rar
-  p7zip-full
-  lzma
-  lzma-dev
-  rar
-  unrar-free
-  p7zip
-  ark
-  ncompress
-  qt5-default
-  vlc
-  vlc-l10n
-  gdebi-core
-  gparted
-  inkscape
-  audacity
-  libdvd-pkg
-  libreoffice
-  libreoffice-styl*
-  libreoffice-l10n-pt-br
-  firefox-locale-pt
-  libavcodec-extra
-  ttf-mscorefonts-installer
-  ubuntu-restricted-extras
-  apt-transport-https
-  ffmpeg
-  libavcodec-extra
-  libdvdcss2
-  gpart
-  build-essential
-  libvulkan1
-  libvulkan1:i386
-  libgnutls30:i386
-  libldap-2.4-2:i386
-  libgpg-error0:i386
-  libxml2:i386
-  libasound2-plugins:i386
-  libsdl2-2.0-0:i386
-  libfreetype6:i386
-  libdbus-1-3:i386
-  libsqlite3-0:i386
-  filezilla
-  vim
-  clang
-  cmake
-  ninja-build
-  pkg-config
-  libgtk-3-dev
-  wget
-  git
-  git-flow
-  zsh
-  net-tools
-  php8.0
-  php8.0-intl
-  php8.0-mysql
-  php8.0-sqlite3
-  php8.0-gd
-  nodejs
-  gcc
-  g++
-  make
-  yarn
-  npm
-)
-# ---------------------------------------------------------------------- #
-
-# ----------------------------- REQUISITOS ----------------------------- #
 ## Removendo travas eventuais do apt ##
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
@@ -124,8 +39,11 @@ sudo rm /var/cache/apt/archives/lock
 ## Adicionando/Confirmando arquitetura de 32 bits ##
 sudo dpkg --add-architecture i386
 
-## Atualizando o repositório ##
-sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoclean && sudo apt autoremove -y
+sudo apt update -y
+# Para distros KDE
+sudo pkcon update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y && sudo apt full-upgrade && sudo apt autoremove -y && sudo apt autoclean
 
 sudo apt install curl -y
 sudo apt install software-properties-common -y
@@ -134,6 +52,17 @@ sudo apt install software-properties-common -y
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+# Balena Etcher
+curl -1sLf \
+   'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' \
+   | sudo -E bash
+
+# Visual Studio Code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
 
 ## Adicionando repositórios de terceiros e suporte a Snap (Driver Logitech, Lutris e Drivers Nvidia)##
 sudo add-apt-repository "$PPA_GIMP" -y
@@ -145,11 +74,11 @@ wget -nc "$URL_WINE_KEY"
 sudo apt-key add winehq.key
 sudo apt-add-repository "deb $URL_PPA_WINE focal main"
 
-# ---------------------------------------------------------------------- #
-
-# ----------------------------- EXECUÇÃO ----------------------------- #
-## Atualizando o repositório depois da adição de novos repositórios ##
-sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoclean && sudo apt autoremove -y
+sudo apt update -y
+# Para distros KDE
+sudo pkcon update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y && sudo apt full-upgrade && sudo apt autoremove -y && sudo apt autoclean
 
 ## Download e instalaçao de programas externos ##
 mkdir "$DIRETORIO_DOWNLOADS"
@@ -161,62 +90,104 @@ wget -c "$URL_VCODE" -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_SKYPE" -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_WORKB" -P "$DIRETORIO_DOWNLOADS"
 
+## Experimental, não sei se vai funcionar
+sudo apt install $DIRETORIO_DOWNLOADS/*.deb -y
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
-
-# Instalar programas no apt
-for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
-  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
-    apt install "$nome_do_programa" -y
-  else
-    echo "[INSTALADO] - $nome_do_programa"
-  fi
-done
-
 sudo apt install -f
 
+sudo apt update -y
+# Para distros KDE
+sudo pkcon update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y && sudo apt full-upgrade && sudo apt autoremove -y && sudo apt autoclean
+
+# As instalações estão separados para em caso de erro de algum pacote, os outros não deixem de ser instalados
+sudo apt install apt-transport-https -y
+sudo apt install build-essential -y
 sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
-
+# Para Linux Mint:
+sudo apt install mint-meta-codecs -y
+sudo apt install dos2unix -y
+sudo apt install language-pack-gnome-pt language-pack-kde-pt language-pack-pt-base -y
+sudo apt install winff -y
+sudo apt install guvcview -y
+sudo apt install flameshot -y
+sudo apt install steam-installer steam-devices steam:i386 -y
+sudo apt install ratbagd -y
+sudo apt install gimp gimp-gmic gmic gimp-plugin-registry -y
+sudo apt install p7zip-rar p7zip-full lzma lzma-dev rar unrar-free p7zip ark ncompress -y
+sudo apt install qt5-default -y
+sudo apt install vlc vlc-l10n -y
+sudo apt install gdebi-core -y
+sudo apt install gparted -y
+sudo apt install inkscape -y
+sudo apt install audacity -y
+sudo apt install libreoffice libreoffice-l10n-pt-br -y
+sudo apt install libreoffice-styl* -y
+sudo apt install firefox-locale-pt -y
+sudo apt install ttf-mscorefonts-installer -y
+sudo apt install ubuntu-restricted-extras -y
+# plugins de audio e vídeo
+sudo apt install libdvd-pkg ffmpeg libavcodec-extra libdvdcss2 -y
 sudo dpkg-reconfigure libdvd-pkg
+########
+sudo apt install gpart -y
+sudo apt install libvulkan1 libvulkan1:i386 libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 -y
+sudo apt install libsqlite3-0:i386 -y
+sudo apt install filezilla -y
+sudo apt install vim -y
+sudo apt install clang -y
+sudo apt install cmake -y
+sudo apt install ninja-build -y
+sudo apt install pkg-config -y
+sudo apt install libgtk-3-dev -y
+sudo apt install wget -y
+sudo apt install git git-flow -y
+sudo apt install zsh -y
+sudo apt install net-tools -y
+sudo apt install php8.0 php8.0-mysql php8.0-sqlite3 php8.0-gd -y
+sudo apt install nodejs -y
+sudo apt install gcc -y
+sudo apt install g++ -y
+sudo apt install make -y
+sudo apt install yarn -y
+sudo apt install balena-etcher-electron -y
+sudo apt install code -y # or code-insiders
 
-sudo rm /etc/apt/preferences.d/nosnap.pref
+## Configurando VSCode como editor padrão
+sudo update-alternatives --set editor /usr/bin/code
+# # Se o Visual Studio Code não aparecer como alternativa ao editor, você precisará registrá-lo:
+# sudo update-alternatives --install /usr/bin/editor editor $(which code) 10
 
-sudo apt install snapd
+## Parar e remover serviço apache instalado com PHP 8 #
+sudo /etc/init.d/apache2 stop && sudo systemctl stop apache2.service && sudo systemctl stop apache2 && sudo systemctl disable apache2 && sudo apt remove apache2 -y
 
-# ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
-## Finalização, atualização e limpeza##
+sudo apt update -y
+# Para distros KDE
+sudo pkcon update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y && sudo apt full-upgrade && sudo apt autoremove -y && sudo apt autoclean
 
-## Instalando pacotes Snap ##
-sudo snap install termius-app
-sudo snap install flutter --classic
+## Atalho Nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 
-## Instalando pacotes Flatpak ##
-flatpak install flathub com.obsproject.Studio -y
+## Instalacao Composer #
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+composer self-update
+
+# Caso queira instalar o snap descomente as linhas abaixo
+# sudo rm /etc/apt/preferences.d/nosnap.pref
+# sudo apt install snapd
+
 flatpak install flathub com.getpostman.Postman -y
-flatpak install flathub com.spotify.Client -y
-flatpak install flathub com.google.AndroidStudio -y
-
-# ---------------------------------------------------------------------- #
-
-sudo apt install -f
-
-# Balena Etcher
-curl -1sLf \
-  'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' |
-  sudo -E bash
-sudo apt-get update
-sudo apt-get install balena-etcher-electron
-
-# Atualizações
-sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoclean && sudo apt autoremove -y
 flatpak update
-sudo snap refresh
-# ---------------------------------------------------------------------- #
 
-## Configuracoes do flutter #
-flutter sdk-path
-flutter config --no-analytics
-flutter upgrade
+## Apagando pasta de Downloads #
+sudo rm -rf "$DIRETORIO_DOWNLOADS"
 
 ## Configuracao GIT #
 git config --global user.name "$GIT_USER"
@@ -227,65 +198,44 @@ ssh-keygen -t ed25519 -C "$GIT_EMAIL"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
-## Instalacao Composer #
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
-composer self-update
+# ## Para instalar o XAMPP 5.5.38 descomente as linhas abaixo#
+# wget https://ufpr.dl.sourceforge.net/project/xampp/XAMPP Linux/5.5.38/xampp-linux-x64-5.5.38-3-installer.run
+# sudo chmod 755 xampp-linux-x64-5.5.38-3-installer.run
+# sudo ./xampp-linux-x64-5.5.38-3-installer.run --installer-language pt_BR
+# sudo rm xampp-linux*
+# sudo ln -s /opt/lampp/htdocs/ $PASTA_USUARIO/htdocs
+# sudo chmod 777 -R $PASTA_USUARIO/htdocs
+# sudo sed -i 's/display_errors=On/display_errors=Off/g' /opt/lampp/etc/php.ini
+# sudo sed -i 's/short_open_tag=Off/short_open_tag=On/g' /opt/lampp/etc/php.ini
+# rm -f $PASTA_USUARIO/.local/share/applications/XAMPP.desktop
+# rm -f $PASTA_USUARIO/Desktop/XAMPP.desktop
+# rm -f "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
+# sudo rm -f /usr/share/applications/XAMPP.desktop
 
-## Parar e remover serviço apache instalado com PHP 8 #
-sudo /etc/init.d/apache2 stop
-sudo systemctl stop apache2.service
-sudo systemctl stop apache2
-sudo systemctl disable apache2
-sudo apt remove apache2 -y
+# cat >"$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" <<EOT
+# [Desktop Entry]
+# Encoding=UTF-8
+# Type=Application
+# Name=XAMPP Control Panel
+# Comment=Start and Stop XAMPP
+# Exec=sudo /opt/lampp/manager-linux-x64.run
+# Icon=/opt/lampp/htdocs/favicon.ico
+# Categories=Development
+# Type=Application
+# Terminal=true
+# EOT
 
-## Instalacao XAMPP 5.6 #
-if [ ! -d "/opt/lampp/etc" ]; then
-  wget https://ufpr.dl.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.40/xampp-linux-x64-5.6.40-1-installer.run
-  sudo chmod 755 xampp-linux-x64-5.6.40-1-installer.run
-  sudo ./xampp-linux-x64-5.6.40-1-installer.run --mode text --installer-language pt_BR
-  sudo rm xampp-linux*
-  sudo ln -s /opt/lampp/htdocs/ $PASTA_USUARIO/htdocs
-  sudo chmod 777 -R $PASTA_USUARIO/htdocs
-  sudo sed -i 's/display_errors=On/display_errors=Off/g' /opt/lampp/etc/php.ini
-  sudo sed -i 's/short_open_tag=Off/short_open_tag=On/g' /opt/lampp/etc/php.ini
-  rm -f $PASTA_USUARIO/.local/share/applications/XAMPP.desktop
-  rm -f $PASTA_USUARIO/Desktop/XAMPP.desktop
-  rm -f "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
-  sudo rm -f /usr/share/applications/XAMPP.desktop
-  cat >"$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" <<EOT
-[Desktop Entry]
-Encoding=UTF-8
-Type=Application
-Name=XAMPP Control Panel
-Comment=Start and Stop XAMPP
-Exec=sudo /opt/lampp/manager-linux-x64.run
-Icon=/opt/lampp/htdocs/favicon.ico
-Categories=Development
-Type=Application
-Terminal=true
-EOT
-  chown $USER:$USER -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
-  chmod 755 -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
-  ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" $PASTA_USUARIO/Desktop/XAMPP.desktop
-  ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
-  sudo cp "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" /usr/share/applications/XAMPP.desktop
-  sudo chmod 755 -R /usr/share/applications/XAMPP.desktop
-  echo "XAMPP Instalado com sucesso!"
-  ## Crinado Link simbólico Mysql #
-  sudo ln -s /opt/lampp/bin/mysql /usr/bin/mysql
-# sudo /opt/lampp/lampp start
-# xdg-open http://localhost
-else
-  echo "Já exite uma instalação do XAMPP"
-fi
+# chown $USER:$USER -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
+# chmod 755 -R "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop"
+# ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" $PASTA_USUARIO/Desktop/XAMPP.desktop
+# ln -s "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" "$PASTA_USUARIO/Área de Trabalho/XAMPP.desktop"
+# sudo cp "$PASTA_USUARIO/.local/share/applications/XAMPP.desktop" /usr/share/applications/XAMPP.desktop
+# sudo chmod 755 -R /usr/share/applications/XAMPP.desktop
+# echo "XAMPP Instalado com sucesso!"
+# ## Crinado Link simbólico Mysql #
+# sudo ln -s /opt/lampp/bin/mysql /usr/bin/mysql
 
-## Apagando pasta de Downloads #
-sudo rm -rf "$DIRETORIO_DOWNLOADS"
-
-clear
+# #### clear
 echo "Adicione as informações abaixo ao seu github:"
 echo ""
 cat ~/.ssh/id_ed25519.pub
