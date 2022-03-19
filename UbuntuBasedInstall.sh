ac6c779067c9df5e60f7b139e957899d040969c4
alvesbeto <beto.souza@corbee.com.br>
2022-03-19 15:09:07 -0300

    instalaçao soptify

--- UbuntuBasedInstall.sh
+++ UbuntuBasedInstall.sh
@@ -168,6 +168,11 @@ sudo apt install yarn -y
 sudo apt install balena-etcher-electron -y
 sudo apt install code -y # or code-insiders
 
+# instalação Spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client -y
+
 ## Configurando VSCode como editor padrão
 sudo update-alternatives --set editor /usr/bin/code
 # # Se o Visual Studio Code não aparecer como alternativa ao editor, você precisará registrá-lo:
