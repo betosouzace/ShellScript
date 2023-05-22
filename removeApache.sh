25d9a157db5d6b7e479aa3bf0f55ee970003c33c
betosouzace <aldemberto@gmail.com>
2023-05-22 10:33:07 -0300

    remoção apache

--- removeApache.sh
+++ removeApache.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ $EUID -eq 0 ]]; then
-  clear
-  echo "                       !!! ATENÇÃO !!!"
-  echo "              NÃO execute este script como root!"
-  echo "              Presione qualquer tecla para sair..."
-  read -s -n 1 -p " "
-  exit
-fi
-
-# ----------------------------- VARIÁVEIS ----------------------------- #
-
 ## Parar e remover serviço apache instalado com PHP 8 #
 sudo /etc/init.d/apache2 stop
 sudo systemctl stop apache2.service
