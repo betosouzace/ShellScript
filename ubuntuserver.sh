3c277dfb1cb2fae35e2cd8c6212ecf0fc08a659e
betosouzace <aldemberto@gmail.com>
2023-08-25 01:56:58 -0300

    ubuntuserver

--- ubuntuserver.sh
+++ ubuntuserver.sh
@@ -1,54 +1,54 @@
 #!/usr/bin/env bash
 
-## Adicionando/Confirmando arquitetura de 32 bits ##
-sudo dpkg --add-architecture i386
+# ## Adicionando/Confirmando arquitetura de 32 bits ##
+# sudo dpkg --add-architecture i386
 
-sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoclean && sudo apt autoremove -y
+# sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoclean && sudo apt autoremove -y
 
-sudo apt install curl -y
-sudo apt install software-properties-common -y
-sudo apt install apt-transport-https -y
-sudo apt-get install ca-certificates wget curl lsb-release -y
-sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common gnupg2 -y
-sudo apt install build-essential -y
-sudo apt install dos2unix -y
-sudo apt install p7zip-rar p7zip-full lzma lzma-dev rar unrar-free p7zip ark ncompress -y
-sudo apt install network-manager-openvpn -y
-sudo apt install neovim -y
-sudo apt install clang -y
-sudo apt install cmake -y
-sudo apt install wget -y
-sudo apt install git git-flow -y
-sudo apt install zsh -y
-sudo apt install net-tools -y
-sudo apt install samba -y
-sudo apt install xrdp -y
-sudo apt install gcc -y
-sudo apt install g++ -y
-sudo apt install make -y
-sudo apt-get install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
+# sudo apt install curl -y
+# sudo apt install software-properties-common -y
+# sudo apt install apt-transport-https -y
+# sudo apt-get install ca-certificates wget curl lsb-release -y
+# sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common gnupg2 -y
+# sudo apt install build-essential -y
+# sudo apt install dos2unix -y
+# sudo apt install p7zip-rar p7zip-full lzma lzma-dev rar unrar-free p7zip ark ncompress -y
+# sudo apt install network-manager-openvpn -y
+# sudo apt install neovim -y
+# sudo apt install clang -y
+# sudo apt install cmake -y
+# sudo apt install wget -y
+# sudo apt install git git-flow -y
+# sudo apt install zsh -y
+# sudo apt install net-tools -y
+# sudo apt install samba -y
+# sudo apt install xrdp -y
+# sudo apt install gcc -y
+# sudo apt install g++ -y
+# sudo apt install make -y
+# sudo apt-get install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
 
-# instalação PHP
-sudo add-apt-repository ppa:ondrej/php -y
-sudo add-apt-repository ppa:ondrej/apache2 -y
-sudo apt update
-sudo apt install openssl mcrypt php8.2 php8.2-mcrypt php8.2-common php8.2-mysql php8.2-sqlite3 php8.2-dom php8.2-bcmath php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip php8.2-intl php8.2-cgi php8.2-pgsql php8.2-ldap -y
-sudo apt install php8.2-{mcrypt,common,mysql,sqlite3,dom,bcmath,xml,xmlrpc,curl,gd,imagick,cli,dev,imap,mbstring,opcache,soap,zip,intl,cgi,pgsql,ldap} -y
-sudo apt install php8.2 php8.2-mysql php8.2-sqlite3 php8.2-dom php8.2-bcmath php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip php8.2-intl php8.2-cgi php8.2-pgsql php8.2-ldap -y
+# # instalação PHP
+# sudo add-apt-repository ppa:ondrej/php -y
+# sudo add-apt-repository ppa:ondrej/apache2 -y
+# sudo apt update
+# sudo apt install openssl mcrypt php8.2 php8.2-mcrypt php8.2-common php8.2-mysql php8.2-sqlite3 php8.2-dom php8.2-bcmath php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip php8.2-intl php8.2-cgi php8.2-pgsql php8.2-ldap -y
+# sudo apt install php8.2-{mcrypt,common,mysql,sqlite3,dom,bcmath,xml,xmlrpc,curl,gd,imagick,cli,dev,imap,mbstring,opcache,soap,zip,intl,cgi,pgsql,ldap} -y
+# sudo apt install php8.2 php8.2-mysql php8.2-sqlite3 php8.2-dom php8.2-bcmath php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip php8.2-intl php8.2-cgi php8.2-pgsql php8.2-ldap -y
 
-# Definindo zsh como shell padrão
-chsh -s $(which zsh)
-chsh -s /usr/bin/zsh
-sudo usermod -s /usr/bin/zsh $(whoami)
-chsh -s /bin/zsh
-sudo chsh -s /bin/zsh
+# # Definindo zsh como shell padrão
+# chsh -s $(which zsh)
+# chsh -s /usr/bin/zsh
+# sudo usermod -s /usr/bin/zsh $(whoami)
+# chsh -s /bin/zsh
+# sudo chsh -s /bin/zsh
 
-## Instalacao Composer #
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
-composer self-update
+# ## Instalacao Composer #
+# php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+# php composer-setup.php
+# php -r "unlink('composer-setup.php');"
+# sudo mv composer.phar /usr/local/bin/composer
+# composer self-update
 
 # configuração do apache
 sudo a2enmod rewrite
