5d014559999f840a95ef2971dd39d2c0fe3c906b
betosouzace <aldemberto@gmail.com>
2023-09-21 20:25:03 -0300

    ubuntuServer

--- ubuntuServer.sh
+++ ubuntuServer.sh
@@ -39,7 +39,6 @@ sudo apt-get update
 
 # Instalando o php 8.2
 sudo apt install openssl mcrypt php8.2 php8.2-mcrypt php8.2-common php8.2-mysql php8.2-sqlite3 php8.2-dom php8.2-bcmath php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip php8.2-intl php8.2-cgi php8.2-pgsql php8.2-ldap -y
-sudo apt install php8.2-{mcrypt,common,mysql,sqlite3,dom,bcmath,xml,xmlrpc,curl,gd,imagick,cli,dev,imap,mbstring,opcache,soap,zip,intl,cgi,pgsql,ldap} -y
 
 ## Instalacao Composer #
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -47,6 +46,15 @@ php composer-setup.php
 php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
 composer self-update
+
+# instala o certbot e o certbot-apache para gerar o certificado SSL
+sudo apt install certbot python3-certbot-apache -y
+
+# Instalando o mysql
+sudo apt install mysql-server -y
+
+# gera a senha do mysql
+sudo mysql_secure_installation
 
 # Atualizando o sistema
 sudo apt update -y
