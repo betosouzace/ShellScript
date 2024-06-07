51dd516c87013ab64151e77ba6e0db20a698b9e0
betosouzace <aldemberto@gmail.com>
2024-06-07 11:12:34 -0300

    postman

--- postman.sh
+++ postman.sh
@@ -20,9 +20,9 @@ sudo tar -xzf /tmp/postman.tar.gz -C /opt
 
 sudo chmod -R 777 /opt/Postman
 
-sudo ln -s /opt/Postman/Postman /usr/bin/postman
+sudo ln -s /opt/Postman/app/Postman /usr/bin/postman
 
-echo -e '[Desktop Entry]\n Version=1.0\n Type=Application\n Name=Postman\n Exec=/usr/bin/postman\n Icon=/opt/Postman/app/resources/app/assets/icon.png\n Categories=Development;Code;\n Terminal=false' | sudo tee /usr/share/applications/postman.desktop
+echo -e '[Desktop Entry]\n Encoding=UTF-8\n Name=Postman\n Exec=/opt/Postman/app/Postman %U\n Icon=/opt/Postman/app/resources/app/assets/icon.png\n Terminal=false\n Type=Application\n Categories=Development;' | sudo tee /usr/share/applications/postman.desktop
 
 echo " "
 echo "Instalação concluída"
