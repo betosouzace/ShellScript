c5adcf85f251296726378ce2eba54409bc077fdf
betosouzace <aldemberto@gmail.com>
2025-07-24 11:09:56 -0300

    .

--- UbuntuBasedInstall.sh
+++ UbuntuBasedInstall.sh
@@ -200,18 +200,7 @@ rm -f "$PASTA_USUARIO/postman-linux-x64.tar.gz"
 echo "Postman instalado com sucesso!"
 
 # Docker
-sudo apt-get remove docker docker-engine docker.io containerd runc -y
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg lsb-release -y
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  noble stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-sudo groupadd docker
-sudo usermod -aG docker $USER
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/betosouzace/ShellScript/master/docker.sh)"
 
 # instalação Spotify
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
